5ee6c1a38209a43bb2bd07293329a86a543053f3
Nat Pavasant <innocenat@server.fake>
2014-01-30 09:59:55 +0700

    Linux: add .gitignore and add Linux in config.h

--- avs_core/include/avs/config.h
+++ avs_core/include/avs/config.h
@@ -44,12 +44,21 @@
 // builds possible.
 #define FRAME_ALIGN 32
 
-#ifdef _M_AMD64
-  #define X86_64
-#elif  _M_IX86
-  #define X86_32
+#if   defined(_M_AMD64) || defined(__x86_64)
+#   define X86_64
+#elif defined(_M_IX86) || defined(__i386__)
+#   define X86_32
 #else
-  #error Unsupported CPU architecture.
+#   error Unsupported CPU architecture.
+#endif
+
+#ifdef _WIN32
+#   define AVS_WINDOWS
+#elif  __linux__
+#   define AVS_LINUX
+#else
+#   error Operating system unsupported.
 #endif
 
 #endif //AVS_CONFIG_H
+
